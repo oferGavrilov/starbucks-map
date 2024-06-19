df723509755cb9e7634beb76b1222fb35ee3e921
Ofer <ofergavri@gmail.com>
2024-06-19 17:52:00 +0300

    start validation feature on client side

---
+++ client/js/validation.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const validateButton = document.getElementById("validate-button");
+    const validationResult = document.getElementById("validation-result");
+
+    validateButton.addEventListener("click", () => {
+        const latitude = parseFloat(document.getElementById("latitude").value);
+    })
+});
